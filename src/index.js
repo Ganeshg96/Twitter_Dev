e6b0438e14776e3a86003f7b2d9bc80a5b91e953
ganesh gone <ganeshgone2003@gmail.com>
2024-07-23 16:54:09 +0530

    added codes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const connect= require('./config/database');
+const app= express();
+
+const TweetRepository=require('./repository/tweet-repository');
+const Comment= require('./models/comment');
+
+app.listen(3000, async() =>{
+    console.log('server started');
+    await connect();
+    console.log('Mongo db connected');
+    // const tweet= await Tweet.create({
+    //     content:'third tweet'    
+    // });
+    //const tweets= await Tweet.find({userEmail:'a@bc.com'});
+    // const tweet= await Tweet.findById('669f79e88974cc34a4ff4c54');
+    // tweet.userEmail='b@c.com';
+    // await tweet.save();
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.getWithComments('669f8f799417a18987578bb6');
+    console.log(tweet);
+});
