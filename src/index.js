acb30af977ab5194b489d1cb2404d0733cea09eb
ganesh gone <ganeshgone2003@gmail.com>
2024-09-08 03:01:41 +0530

    added passport logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {connect} from './config/database.js';
+import passport from 'passport';
+import { passportAuth } from './config/jwt-middleware.js';
 
 import apiRoutes from './routes/index.js';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
+
 app.use('/api', apiRoutes);
+
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
